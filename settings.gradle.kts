ef2454110bd755434b3c7ec1635eefb9b46dca93
Ballbot <5252bb@daum.net>
2024-03-17 11:50:28 +0900

    0317

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KNU_Lecture_2024_01_Android"
+include(":app")
+ 
